10c01e89010dc95183a419077be92111090e2db8
Zaina-coder <81202142+Zaina-coder@users.noreply.github.com>
2021-06-05 10:07:58 -0700

    Change the loader color

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather (props){
         hello 
         <Loader
         type="Puff"
-        color="#00BFFF"
+        color="#BFAE3F"
         height={100}
         width={100}
         timeout={3000} //3 secs
